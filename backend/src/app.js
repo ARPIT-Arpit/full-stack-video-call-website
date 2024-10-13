ff41c4e902c9d9d769fcd414dc60a039c3869332
ARPIT <arpit2k4@gmail.com>
2024-10-13 22:16:09 +0530

    first commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,9 +13,11 @@ const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
+app.use(cors({
+    origin: "https://full-stack-video-call-website.onrender.com";
+}));
 
 app.set("port", (process.env.PORT || 8000))
-app.use(cors());
 app.use(express.json({limit: "40kb"}));
 app.use(express.urlencoded({limit: "40kb", extended: true}));
 
